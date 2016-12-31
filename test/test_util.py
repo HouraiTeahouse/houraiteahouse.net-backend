ef9544448f0955e89d922948519fe8c460aad93d
james7132 <james.siqi.liu@gmail.com>
2016-12-31 16:19:17 -0500

    Move flask_test_util
    For better isolation of tests and source

--- test/test_util.py
+++ test/test_util.py
@@ -18,5 +18,11 @@ class HouraiTeahouseTestCase(TestCase):
         db.session.remove()
         db.drop_all()
 
+    def test_test(self):
+        pass
+
+    def test_test_test(self):
+        pass
+
 if __name__ == "__main__":
     unittest.main()
